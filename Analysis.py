93714cf9561f231b6acb8b6c21c8976a52ab3d2c
Jamie <jamietohall@hotmail.com>
2022-04-15 18:25:06 +0100

    Fishers Iris Data Set

---
+++ Analysis.py
@@ -0,0 +1,9 @@
+# Programming and Scripting final Project - 2022
+
+# This project concerns the well-known Fisher’s Iris data set. You must research the data set and 
+# write documentation and code to investigate it.
+# The programme must output a summary of each variable to a single text file, Save a histogram of each
+# variable to png files and output a scatterplot of each pair of variables. Perform any other analysis
+# you think is appropriate. 
+
+# Author: Jamie Tohall
