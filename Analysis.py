ea250fabc6a07afec85c7285ec5c0400f132c534
Jamie <jamietohall@hotmail.com>
2022-04-21 20:05:10 +0100

    Iris Data Set

--- Analysis.py
+++ Analysis.py
@@ -1,9 +1,12 @@
-# Programming and Scripting final Project - 2022
+# Programming and Scripting Final Project - 2022
 
-# This project concerns the well-known Fisher’s Iris data set. You must research the data set and 
-# write documentation and code to investigate it.
-# The programme must output a summary of each variable to a single text file, Save a histogram of each
-# variable to png files and output a scatterplot of each pair of variables. Perform any other analysis
-# you think is appropriate. 
+# This project concerns the well-known Fisher’s Iris data set. You must research the data set and write documentation and code to investigate it.
+# The programme must output a summary of each variable to a single text file, Save a histogram of each variable to png files and output a scatterplot of each pair of variables. 
+# Perform any other analysis you think is appropriate. 
 
 # Author: Jamie Tohall
+
+import pandas
+import seaborn
+import matplotlib.pyplot
+import numpy 
